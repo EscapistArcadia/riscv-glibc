ba49b40ca7b1b4daca6aafe1c3791de2902600ce
Shanbo Zhang <EscapistArcadia@outlook.com>
2026-02-21 16:42:27 -0600

    Add functions to set accelerators' attribute

---
+++ nptl/pthread_attr_setsmqueue.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdint.h>
+#include "pthreadP.h"
+
+int __pthread_attr_setsmqueue_np(pthread_attr_t *attr, uint64_t queue_ptr) {
+    assert(sizeof (*attr) >= sizeof (struct pthread_attr));
+
+    struct pthread_attr *iattr = (struct pthread_attr *) attr;
+    
+    iattr->accel.queue_ptr = queue_ptr;
+    iattr->flags |= ATTR_FLAG_ACCELERATOR;
+    return 0;
+}
+strong_alias(__pthread_attr_setsmqueue_np, pthread_attr_setsmqueue_np)
